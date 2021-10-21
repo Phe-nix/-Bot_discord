142f54392bdbe204ae82d46a669d7a3d6348fbae
Phe-nix <88503925+Phe-nix@users.noreply.github.com>
2021-10-21 15:43:57 +0700

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,3 +1,5 @@
+import asyncio
+from asyncio.tasks import wait
 import discord
 from discord.ext import commands
 
@@ -10,5 +12,29 @@ async def ping(): # เช็คว่า bot มันกำลังทำง�
 @client.command()
 async def ping(ctx): # ฟังชันก์ เช็คปิง ของ ผู้ใช้ (ยังมีปัญหา)
     await ctx.send(f'Ping: {round(client.latency * 1000)} ms')
+    # check spammer 
+@client.event
+async def on_ready():
+    print("ready")
+    while True:
+        print("cleanred")
+        await asyncio.sleep(10)
+        with open("spam_detect.txt", "r+") as file:
+            file.truncate(0)
 
+@client.event
+async def on_message(message):
+    counter = 0
+    with open("spam_detect.txt", "r+") as file:
+        for lines in file:
+            if lines.strip("\n") == str(message.author.id):
+                counter += 1
+
+        file.writelines(f"{str(message.author.id)}\n")
+        if counter > 5:
+            await message.guild.ban(message.author, reason="spam")
+            await asyncio.sleep(1)
+            await message.guild.unban(message.author)
+            print("u are doommmmmm!!")
+            
 client.run('TOKEN') # TOKEN ของ Bot
