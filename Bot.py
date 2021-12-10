ce00f8a2305f4ea32fed1a82afa897bee86081bf
NutThai <88415649+NutThai@users.noreply.github.com>
2021-12-10 16:53:34 +0700

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -37,11 +37,11 @@ class main(commands.Cog):
         # Bot does not have permission
         elif isinstance(error, commands.MissingPermissions):
             await ctx.send('Bot Permission Missing!')
-#ลบข้อความ nข้อความ
-@client.command()
-async def clear(ctx, amount=5):
-    await ctx.channel.purge(limit=amount)
-    await ctx.channel.send("ข้อความถูกลบเรียบร้อยแล้ว", delete_after=5)
+    #ลบข้อความ nข้อความ
+    @commands.command()
+    async def clear(self, ctx, amount=5):
+        await ctx.channel.purge(limit=amount)
+        await ctx.channel.send("ข้อความถูกลบเรียบร้อยแล้ว", delete_after=5)
 
 if __name__ == '__main__':
     # Load extension
